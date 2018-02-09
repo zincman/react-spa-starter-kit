4758e9702e219e73aecd2898f8a6dbca6c51997e
Glucose <lyd.derek.lu@gmail.com>
2018-02-09 14:13:47 +0800

    Update postcss.config.js
    add precss warning

--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,7 @@ module.exports = {
 		// Import styles in compile time and allow hmr for @import
 		require('postcss-import'),
 		// Allow to use Sass-like markup
+		// warning: conflict with custom properties set & @apply in cssnext
 		require('precss'),
 		// The plugin to future CSS standard syntax
 		require('postcss-cssnext'),
